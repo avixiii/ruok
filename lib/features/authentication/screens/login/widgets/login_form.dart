41cec2d33c7649d423dc52c6cd58974b726ac5d8
avi <avixiii@outlook.com>
2023-10-20 18:11:22 +0700

    Update UI: only show remember_me on the web

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -6,6 +6,7 @@ import 'package:ruok/features/authentication/screens/signup/signup.dart';
 import 'package:ruok/navigation_menu.dart';
 import 'package:ruok/utils/constants/sizes.dart';
 import 'package:ruok/utils/constants/text_strings.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class LoginForm extends StatelessWidget {
   const LoginForm({
@@ -16,70 +17,72 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Form(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-              vertical: Sizes.spaceBtwSections),
-          child: Column(
-            children: [
-              // Email
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Iconsax.direct_right),
-                    hintText: Texts.emailHint,
-                    labelText: Texts.email),
-              ),
-              const SizedBox(height: Sizes.spaceBtwInputFields),
+      padding: const EdgeInsets.symmetric(vertical: Sizes.spaceBtwSections),
+      child: Column(
+        children: [
+          // Email
+          TextFormField(
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.direct_right),
+                hintText: Texts.emailHint,
+                labelText: Texts.email),
+          ),
+          const SizedBox(height: Sizes.spaceBtwInputFields),
 
-              // Password
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Iconsax.password_check),
-                    hintText: Texts.password,
-                    labelText: Texts.password),
-              ),
-              const SizedBox(height: Sizes.spaceBtwInputFields / 2),
+          // Password
+          TextFormField(
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                hintText: Texts.password,
+                labelText: Texts.password),
+          ),
+          const SizedBox(height: Sizes.spaceBtwInputFields / 2),
 
-              // Remember Me & Forget Password
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Remember Me
-                  const Row(
-                    children: [
-                      Checkbox(value: true, onChanged: null),
-                      Text(Texts.rememberMe)
-                    ],
-                  ),
-                  // Forget Password
-                  TextButton(
-                      onPressed: () {
-                        Get.to(() => const ForgetPassword());
-                      },
-                      child: const Text(Texts.forgetPassword))
-                ],
-              ),
-              const SizedBox(height: Sizes.spaceBtwSections),
-
-              // Sign In Button
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(() => const NavigationMenu());
-                    }, child: const Text(Texts.signIn)),
-              ),
-              const SizedBox(height: Sizes.spaceBtwItems),
-              // Create Account Button
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(
-                  onPressed: () {
-                    Get.offAll(() => const SignUpScreen());
-                  },
-                  child: const Text(Texts.createAccount),
+          // Remember Me & Forget Password
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              // Remember Me
+              if (kIsWeb)
+                const Row(
+                  children: [
+                    Checkbox(value: true, onChanged: null),
+                    Text(Texts.rememberMe)
+                  ],
                 ),
-              )
+              const Spacer(),
+              // Forget Password
+              TextButton(
+                  onPressed: () {
+                    Get.to(() => const ForgetPassword());
+                  },
+                  child: const Text(Texts.forgetPassword))
             ],
           ),
-        ));
+          const SizedBox(height: Sizes.spaceBtwSections),
+
+          // Sign In Button
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+                onPressed: () {
+                  Get.to(() => const NavigationMenu());
+                },
+                child: const Text(Texts.signIn)),
+          ),
+          const SizedBox(height: Sizes.spaceBtwItems),
+          // Create Account Button
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton(
+              onPressed: () {
+                Get.offAll(() => const SignUpScreen());
+              },
+              child: const Text(Texts.createAccount),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
